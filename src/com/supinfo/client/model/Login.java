fb73fbf5582d156625c3fc519b472d5d9edaa1f7
FranckReynaud <160365@supinfo.com>
2014-05-08 16:50:05 -0400

    Class Login possible

---
+++ src/com/supinfo/client/model/Login.java
@@ -0,0 +1,23 @@
+package com.supinfo.client.model;
+
+public abstract class Login {
+	
+	protected String name;
+	protected String password;
+	protected String eMail;
+	
+	//Constructor :
+	protected Login(){
+		
+	}
+	
+	//Methods :
+	
+	private void sendLoginToServer(){
+		
+	}
+	
+	
+	
+	
+}
