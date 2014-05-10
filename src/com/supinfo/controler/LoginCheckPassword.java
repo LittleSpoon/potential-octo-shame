93bc3067df47eba0f441a5a0a542efc344570fb8
Elepole <blackhole.vs.darkhole@gmail.com>
2014-05-10 12:58:51 -0400

    Password number verification
    Stuff about number and password

--- src/com/supinfo/controler/LoginCheckPassword.java
+++ src/com/supinfo/controler/LoginCheckPassword.java
@@ -1,7 +1,10 @@
 package com.supinfo.controler;
 
 import com.supinfo.client.model.Login;
+import com.supinfo.excpetion.PasswordHaveNotNumber;
+import com.supinfo.excpetion.PasswordHaveNotUppercase;
 import com.supinfo.excpetion.PasswordInvalid;
+import com.supinfo.excpetion.PasswordTooShort;
 
 public class LoginCheckPassword extends Login {
 
@@ -20,12 +23,58 @@ public class LoginCheckPassword extends Login {
 		this.password = loginPW;
 	}
 	
-	public void checkPasswordSize() throws PasswordInvalid {
+	public void checkPasswordSize() throws PasswordTooShort {
+		
 		if (password.length() < 8){
-			throw new PasswordInvalid();
+			throw new PasswordTooShort();
 		}
 			
-	}		
+	}
+	
+	public void checkPasswordNumber() throws PasswordHaveNotNumber {
+		char charTest;
+		int numberCounter = 0;
+		
+		for(int i=0;i == password.length(); i++){
+			charTest = password.charAt(i);
+			switch (charTest) {
+			case 1: numberCounter++;
+					break;
+			case 2: numberCounter++;
+					break;
+			case 3: numberCounter++;
+					break;
+			case 4: numberCounter++;
+					break;
+			case 5: numberCounter++;
+					break;
+			case 6: numberCounter++;
+					break;
+			case 7: numberCounter++;
+					break;
+			case 8: numberCounter++;
+					break;
+			case 9: numberCounter++;
+					break;
+			case 0: numberCounter++;
+			default: break;			
+			}
+			
+			if(numberCounter > 0){
+				break;
+			}
+		}
+		
+		if(numberCounter < 0){
+			return;
+		} else {
+			throw new PasswordHaveNotNumber();
+		}
+	}
+	
+	public void checkPasswordUppercase() throws PasswordHaveNotUppercase {
+		
+	}
 	
 	
 
