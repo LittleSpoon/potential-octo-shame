39bd681072006def5302c6918be94db3502c4fee
Elepole <blackhole.vs.darkhole@gmail.com>
2014-05-14 13:52:05 -0400

    Connection class creation

---
+++ src/com/supinfo/controler/connection/LoginConnect.java
@@ -0,0 +1,16 @@
+package com.supinfo.controler.connection;
+
+import com.supinfo.controler.validator.LoginCheckPassword;
+
+public class LoginConnect extends LoginCheckPassword {
+
+	public LoginConnect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginConnect(String loginId, String loginPW) {
+		super(loginId, loginPW);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/supinfo/controler/connection/RegisterConnect.java
@@ -0,0 +1,16 @@
+package com.supinfo.controler.connection;
+
+import com.supinfo.controler.validator.RegisterCheck;
+
+public class RegisterConnect extends RegisterCheck {
+
+	public RegisterConnect() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RegisterConnect(String loginId, String loginPW, String loginEMail) {
+		super(loginId, loginPW, loginEMail);
+		// TODO Auto-generated constructor stub
+	}
+
+}
