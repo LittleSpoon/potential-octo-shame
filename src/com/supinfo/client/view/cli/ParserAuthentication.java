e4ff6236ff673d9c882f4be1a2ab1b69598f544f
FranckReynaud <160365@supinfo.com>
2014-05-14 23:41:35 -0400

    some optimisations of conditions

--- src/com/supinfo/client/view/cli/ParserAuthentication.java
+++ src/com/supinfo/client/view/cli/ParserAuthentication.java
@@ -4,6 +4,9 @@ public class ParserAuthentication {
 	
 	String[] temp;
 	
+	
+	//CONSTRUCTOR :
+	
 	public ParserAuthentication(String[] arguments){
 		
 		temp = new String[3];
@@ -16,35 +19,39 @@ public class ParserAuthentication {
 			System.out.println("invalid use of command ; enter --help to open help page");
 		}
 		
-		for(int i =0; i < arguments.length -1; i++){
+		else{
 			
-			switch(arguments[i]){
+			for(int i =0; i < arguments.length -1; i++){
 				
-				case "--username":
-					if(arguments[i+1] != null){
-						temp[0] = arguments[i+1];
-					}
+				switch(arguments[i]){
 					
-					break;
-				
-				case "--password":
-					if(arguments[i+1] != null){
-						temp[1] = arguments[i+1];
-					}
-					break;
-				
-				case "--project":
-					if(arguments[i+1] != null){
-						temp[2] = arguments[i+1];
-					}
-					break;
-				
+					case "--username":
+						if(arguments[i+1] != null){
+							temp[0] = arguments[i+1];
+						}
+						
+						break;
+					
+					case "--password":
+						if(arguments[i+1] != null){
+							temp[1] = arguments[i+1];
+						}
+						break;
+					
+					case "--project":
+						if(arguments[i+1] != null){
+							temp[2] = arguments[i+1];
+						}
+						break;
+					
+				}
 			}
 		}
 		
 	}
 	
 	
+	//METHODS :
 	public String[] authenticationValue(){
 		
 		
